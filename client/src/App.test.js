65f0a47837700fd8c9c62dad4666787120e9865b
yishai <yishai5555@gmail.com>
2020-06-26 13:49:21 +0300

    fix-client-test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  // const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Welcome to my Fib application/i);
   expect(linkElement).toBeInTheDocument();
 });
